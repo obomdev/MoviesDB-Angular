12807573d84d7dbd94d10f69081d57935f6b961d
obomdev <vidaldevpro@gmail.com>
2025-05-14 20:32:26 -0300

    Estilizado a página Home, configurado as rotas de link e Fixado a navbar.
    Co-authored-by: KeevinZ <kevin1080p.mdm@gmail.com>

---
+++ MoviesDB/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, RouterLink  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'MoviesDB';
+  navbg: any;
+
+  @HostListener('document:scroll') scrollover(){
+    if(document.body.scrollTop > 100  || document.documentElement.scrollTop > 100){
+      this.navbg = { 'background-color': '#000'}
+    }else{
+      this.navbg ={}
+    }  
+  }
+}
